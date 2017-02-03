94a28697df1e9e925fb235b1c8615e134069ca4a
Grace <gracefctorres@gmail.com>
2017-02-03 16:42:26 -0500

    other method

--- js/main.js
+++ js/main.js
@@ -1,4 +1,49 @@
-var  tarea = document.getElementById("tarea");
+var taskList =[]
+function onButtonClick(){
+    var element =document.getElementById("tarea").value;
+    taskList.push({text:element,isDone:false});
+    if(element===""){
+        return false;
+    }else{
+      return drawTaskList();}
+}
+function drawTaskList(){
+    var list = document.getElementById("lista");
+    lista.innerHTML ="";
+    
+   for(var i in taskList){
+      var html = "<li class='tareas' id='tasks'><input id='check'onclick='onCheckedList("+i+")' type='checkbox' "+(taskList[i].isDone?"checked":"")+">" + "<span>"+ taskList[i].text +"</span><i class='fa fa-trash tachito' onclick='deleteList("+i+")'style='font-size:25px;'></i></li>";
+        lista.innerHTML+=html;}
+}
+function onCheckedList(_checkBox){
+    var list = document.getElementById("lista");
+    if (taskList[_checkBox].isDone==false){
+        taskList[_checkBox].isDone=true;
+        list.childNodes[_checkBox].childNodes[1].style.textDecoration="line-through";
+    }else{
+        taskList[_checkBox].isDone=false;
+        list.childNodes[_checkBox].childNodes[1].style.textDecoration="none";}
+}
+function deleteList(_checkBox){ 
+    taskList.splice(_checkBox,1)
+    drawTaskList();
+}
+/*********************************************************NOTES******************************************************/
+/*var taskList=[
+    {nombre:"hola", isDone:true},
+];
+//funciones ternarias taskList[i][1]
+//SOLO SE HACEN LAS FUNCIONES TERNARIAS CUANDO LOS VALORES SON BOOLEANOS, es decir, solo cuando se escoje entre dos opciones.
+//Ternary functions just work with booleans, can choose between 2 options: false or true.
+function drawTaskList(){
+    var lista=document.getElementById("lista");
+    for(var i in taskList){
+        var html = "<li>< input type='checkbox' "+(taskList[i].isDone?"checked":"")+ "></input>" + taskList[1].nombre+ "</li>";
+        lista.innerHTML += html; 
+    }
+}
+/*****************************************OTHER METHOD - CREATING NODES******************************************************/
+/*var  tarea = document.getElementById("tarea");
 var  button = document.getElementById("agregar");
 var lista = document.getElementById("lista");
 
@@ -36,10 +81,7 @@ function addTarea(){
     borrar.addEventListener('click',ondeleteItem);
     checkBox.addEventListener('click',checkStatus);
 }
-function ondeleteItem(evt){
-    var lista = evt.target.parentNode.parentNode.parentNode;
-    lista.removeChild( evt.target.parentNode.parentNode);   
-}
+
 function checkStatus(evt){
     var check = evt.target.parentNode;
     if(evt.target.checked){
@@ -48,5 +90,5 @@ function checkStatus(evt){
         check.style.textDecoration="none";
     }
 }
-
+*/
 
